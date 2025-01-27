ddff0c194a1ac41330f27fd28fb748703cfe99ce
mennaabdelelhady <menamahmod660@gmail.com>
2025-01-27 22:49:46 +0200

    Route

--- week_13/static.php
+++ week_13/static.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 Class Test{
@@ -19,4 +20,27 @@ class DataBase{
         echo Database::$dbHost.".".Database::$dbName.".".Database::$dbUserName;
     }
 }
+=======
+<?php
+
+Class Test{
+    public $name ='Menna';
+    public static $brand = "BMW";
+
+    
+    public static function welcome(){
+        echo "welcome".$this->$brand;
+    }
+}
+
+class DataBase{
+    public static $dbName = "mysql";
+    public static $dbHost = "localhost";
+    public static $dbUserName = "root";
+    public static function get_connection(){
+        echo "connected to".self::$db;
+        echo Database::$dbHost.".".Database::$dbName.".".Database::$dbUserName;
+    }
+}
+>>>>>>> ad06a688 (Route)
 Database::get_connection();
